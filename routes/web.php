fa8fc9b77b2abc15e06292fd19950e5c8226fa42
InsetJesux <insetjesux@gmail.com>
2023-02-08 21:10:08 +0100

    Cambiar ruta por defecto a la dashboard

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Inertia\Inertia;
 |
 */
 
+/*
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -23,13 +24,14 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
+*/
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('/', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });
