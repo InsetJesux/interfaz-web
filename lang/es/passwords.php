5a7bd2b82ce8951217c5ff0e978936d106d40faa
InsetJesux <insetjesux@gmail.com>
2023-02-10 17:30:51 +0100

    Añadir archivo de localización a español

---
+++ lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Tu contraseña ha sido reestablecida!',
+    'sent' => 'Te hemos enviado un email con un enlace para reestablecer tu contraseña!',
+    'throttled' => 'Espera antes de volver a intentarlo.',
+    'token' => 'Este token para reestablecer la contraseña no es válido.',
+    'user' => "No se ha encontrado un usuario con ese email.",
+
+];
